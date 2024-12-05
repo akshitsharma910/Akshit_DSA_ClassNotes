361277ab7986494d7753b0bcfb2155b2a7cd5ffc
Akshit Sharma <work.akshitsharma@gmail.com>
2024-12-05 17:04:42 +0530

    newbie practice

---
+++ hello.java
@@ -0,0 +1,16 @@
+
+public class hello {
+
+    public static int add(int n1,int n2){
+        return n1+n2;
+    }
+
+    public static void main(String[] args){
+        int arr[]={10,20,30,40,50};
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Element: "+i);
+        }
+
+    }
+}
+
